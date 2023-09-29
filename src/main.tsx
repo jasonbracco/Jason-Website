db1796ef5f3dee8fd10cc5f07dd9733a0e7c72ca
Jason Bracco <jasonbracco@macbook-pro.mynetworksettings.com>
2023-09-29 19:50:44 -0400

    adding in bootstrap accordian feature for experience

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './Styles.css'; // Import your CSS file here
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css' //Bootstrap Css
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; //Bootstrap JavaScript
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
