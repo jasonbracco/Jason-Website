c60b7629aaf9a600414568ba983e20fb7297fc71
Jason Bracco <jasonbracco@macbook-pro.mynetworksettings.com>
2023-09-30 16:34:19 -0400

    classname for each accordiion componenet

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -86,70 +86,35 @@ function Experience(){
     let technicalProjectsArray = [restaurant_review, photo_share];
 
     return (
-/* <div className="accordion" id="accordionExample">
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div> */
-
-
-
         <div>
-            <h3>Work Experience:</h3>
-            {experienceArray.map((experience, index) => (
-                <div className='experience-card' key={index}>
-                    <ExperienceCard experience={experience} />
-                </div>
-            ))
-            }
-            <h3>Education:</h3>
-            {educationArray.map((education, index) => (
-                <div className='education-card' key={index}>
-                    <EducationCard education={education} />
-                </div>
-            ))
-            }
+            <div className="work-accordion">
+                <h3>Work Experience:</h3>
+                {experienceArray.map((experience, index) => (
+                    <div className='experience-card' key={index}>
+                        <ExperienceCard experience={experience} />
+                    </div>
+                ))
+                }
+            </div>
+            <div className="education-accordion">
+                <h3>Education:</h3>
+                {educationArray.map((education, index) => (
+                    <div className='education-card' key={index}>
+                        <EducationCard education={education} />
+                    </div>
+                ))
+                }
+            </div>
             <br></br>
-            <h3>Technical Projects:</h3>
-            {technicalProjectsArray.map((project, index) => (
-                <div className='project-card' key={index}>
-                    <TechnicalProjectCard project={project} />
-                </div>
-            ))
-            }
+            <div className="project-accordion">
+                <h3>Technical Projects:</h3>
+                {technicalProjectsArray.map((project, index) => (
+                    <div className='project-card' key={index}>
+                        <TechnicalProjectCard project={project} />
+                    </div>
+                ))
+                }
+            </div>
         </div>
     )
 }
